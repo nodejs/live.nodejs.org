5db16958e67b1c4ba0b9e46d077d2aae7c490636
xcatliu <xcatliu@gmail.com>
2016-04-22 17:11:06 +0900

    Add Language Support

--- build.js
+++ build.js
@@ -1,24 +1,103 @@
 "use strict"
+
+// Configuration languages, need to create a folder first
+const languages = ['zh-CN'];
+
 const handlebars = require('handlebars')
 const marked = require('marked')
 const fs = require('fs')
 const path = require('path')
 
-let baseTemplate = handlebars.compile(fs.readFileSync(path.join(__dirname, 'base.hbs')).toString())
-let eventTemplate = handlebars.compile(fs.readFileSync(path.join(__dirname, 'event.hbs')).toString())
-let index = fs.readFileSync(path.join(__dirname, 'index.md'))
+const isMarkdown = /\.md/i
 
-module.exports = function () {
-  fs.writeFileSync(path.join(__dirname, 'index.html'), baseTemplate({content:index}))
+const defaultBaseTemplate = handlebars.compile(fs.readFileSync(path.resolve(__dirname, 'base.hbs'), 'utf8'))
+const defaultEventTemplate = handlebars.compile(fs.readFileSync(path.resolve(__dirname, 'event.hbs'), 'utf8'))
+const defaultTopContent = marked(fs.readFileSync(path.resolve(__dirname, 'top.md'), 'utf8'))
+const defaultIndexContent = marked(fs.readFileSync(path.resolve(__dirname, 'index.md'), 'utf8'))
+const defaultEventsFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(isMarkdown.test.bind(isMarkdown))
 
-  let isMarkdown = /\.md/i
+// Build English site
+function buildDefault() {
+  fs.writeFileSync(path.resolve(__dirname, 'index.html'), defaultBaseTemplate({
+    content: defaultIndexContent,
+    top: defaultTopContent,
+    lang: 'en'
+  }))
 
-  fs.readdirSync(path.join(__dirname, 'events')).forEach(function (file) {
-    if (!isMarkdown.test(file)) return // not a markdown file
-    let f = path.join(__dirname, 'events', file)
-    let s = eventTemplate({content: marked(fs.readFileSync(f).toString())})
-    fs.writeFileSync(f.replace('.md', '.html'), baseTemplate({content:s}))
+  defaultEventsFiles.forEach(function (file) {
+    const filePath = path.resolve(__dirname, 'events', file)
+    const eventContent = defaultEventTemplate({content: marked(fs.readFileSync(filePath, 'utf8'))})
+    fs.writeFileSync(filePath.replace('.md', '.html'), defaultBaseTemplate({
+      content: eventContent,
+      top: defaultTopContent,
+      lang: 'en'
+    }))
   })
+}
+
+/**
+ * Build a specific language
+ *
+ * @param {String} A language identification code, such as zh-CN, ja, de
+ *        see http://www.metamodpro.com/browser-language-codes
+ * @returns {undefined} Write files while executing, no returns.
+ */
+function buildLocalization(language) {
+  if (typeof language !== 'string') return
+  const lang = language.trim()
+  if (lang !== language) return
+  if (lang.length === 0) return
+  const langDirPath = path.resolve(__dirname, language)
+  if (!fs.existsSync(langDirPath)) return
+
+  let baseTemplate = defaultBaseTemplate;
+  const baseTemplatePath = path.resolve(langDirPath, 'base.hbs')
+  if (fs.existsSync(baseTemplatePath)) {
+    baseTemplate = handlebars.compile(fs.readFileSync(baseTemplatePath, 'utf8'))
+  }
+
+  let eventTemplate = defaultEventTemplate;
+  const eventTemplatePath = path.resolve(langDirPath, 'event.hbs')
+  if (fs.existsSync(eventTemplatePath)) {
+    eventTemplate = handlebars.compile(fs.readFileSync(eventTemplatePath, 'utf8'))
+  }
+
+  let indexContent = defaultIndexContent;
+  const indexFilePath = path.resolve(langDirPath, 'index.md')
+  if (fs.existsSync(indexFilePath)) {
+    indexContent = marked(fs.readFileSync(indexFilePath, 'utf8'))
+  }
+
+  let topContent = defaultTopContent;
+  const topFilePath = path.resolve(langDirPath, 'top.md')
+  if (fs.existsSync(topFilePath)) {
+    topContent = marked(fs.readFileSync(topFilePath, 'utf8'))
+  }
+
+  fs.writeFileSync(path.resolve(langDirPath, 'index.html'), baseTemplate({
+    content: indexContent,
+    top: topContent,
+    lang: language
+  }))
+
+  defaultEventsFiles.forEach(function (file) {
+    let srcFilePath = path.resolve(langDirPath, 'events', file)
+    const destFilePath = srcFilePath.replace('.md', '.html')
+    if (!fs.existsSync(srcFilePath)) {
+      srcFilePath = path.resolve(__dirname, 'events', file)
+    }
+    const eventContent = eventTemplate({content: marked(fs.readFileSync(srcFilePath, 'utf8'))})
+    fs.writeFileSync(destFilePath, baseTemplate({
+      content: eventContent,
+      top: topContent,
+      lang: language
+    }))
+  })
+}
+
+module.exports = function() {
+  buildDefault()
+  languages.forEach(buildLocalization)
 }
 
 if (require.main === module) {
